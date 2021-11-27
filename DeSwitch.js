63dbb7dbf66096c9127ad12f9c87cf223c312629
Dabin Lee <dabin.lee@outlook.com>
2021-11-28 04:27:38 +0800

    fixed querychainlink

--- DeSwitch.js
+++ DeSwitch.js
@@ -645,22 +645,6 @@ priceFeed.methods.latestRoundData().call()
         );
 })
 
-queryChainlink.onclick = async() =>{
-  priceFeed.methods.latestRoundData().call().then((roundData)=>{
-    console.log("Latest Round Data", roundData);
-        var aa = document.getElementById("chainlinkResult");
-        var formattedTime = new Date(roundData[2]*1000).toLocaleDateString("en-US");
-        var formmatedTime2 = new Date(roundData[2]*1000).toLocaleTimeString("en-US")
-        aa.innerHTML = (
-          "current price of ETH is US$"+
-          (roundData[1]/100000000) +
-          " "+
-          "USD based on Chainlink Oracle, updated at "+
-          formattedTime+
-          formmatedTime2
-        );
-  })
-}
 
 queryBalance.onclick = async () => {
   // const queryAddress = document.getElementById
@@ -676,6 +660,24 @@ queryBalance.onclick = async () => {
       // +result[1]
     );
   });
+}
+
+
+queryChainlink.onclick = async() =>{
+  priceFeed.methods.latestRoundData().call().then((roundData)=>{
+    console.log("Latest Round Data", roundData);
+        var aa = document.getElementById("chainlinkResult");
+        var formattedTime = new Date(roundData[2]*1000).toLocaleDateString("en-US");
+        var formmatedTime2 = new Date(roundData[2]*1000).toLocaleTimeString("en-US")
+        aa.innerHTML = (
+          "current price of ETH is US$"+
+          (roundData[1]/100000000) +
+          " "+
+          "USD based on Chainlink Oracle, updated at "+
+          formattedTime+
+          formmatedTime2
+        );
+  })
 }
 
 //event fired upon clicking "Register Game" button from the browser
